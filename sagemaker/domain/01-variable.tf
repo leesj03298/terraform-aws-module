0b9bbe3d9e771de657af1d3f9fa8b5e63c02c7c3
lsj <leesj03298@naver.com>
2024-01-09 20:42:31 +0900

    Sagemaker Domain Update

--- sagemaker/domain/01-variable.tf
+++ sagemaker/domain/01-variable.tf
@@ -19,15 +19,81 @@ variable "scg_id" {
   type        = map(string)
 }
 
-variable "domain" {
+variable "domain_name" {
+  type = string
+}
+
+variable "auth_mode" {
+  type    = string
+  default = "IAM"
+}
+
+variable "app_network_access_type" {
+  ## app_network_access_type option : PublicInternetOnly, VpcOnly
+  type    = string
+  default = "PublicInternetOnly"
+}
+
+variable "vpc_identifier" {
+  type = string
+}
+
+variable "subnet_identifiers" {
+  type = list(string)
+}
+
+variable "execution_role" {
+  ## execution_role input date type : arn
+  type = string
+}
+
+### Required
+variable "canvas_app_settings" {
   type = object({
-    domain_name             = string
-    auth_mode               = optional(string, "IAM")
-    ## app_network_access_type option : PublicInternetOnly, VpcOnly
-    app_network_access_type = optional(string, "PublicInternetOnly")
-    vpc_identifier          = string
-    subnet_identifiers      = list(string)
-    ## execution_role input date type : arn
-    execution_role          = string
+    model_register_settings = optional(object({
+      cross_account_model_register_role_arn = optional(string, null)
+      status                                = optional(string, null)
+    }), null)
+    time_series_forecasting_settings = optional(object({
+      amazon_forecast_role_arn = optional(string, null)
+      status                   = optional(string, null)
+    }), null)
+    workspace_settings = optional(object({
+      s3_artifact_path = optional(string, null)
+      s3_kms_key_id    = optional(string, null)
+    }), null)
+    direct_deploy_settings = optional(object({
+      status = optional(string, null)
+    }), null)
+    identity_provider_oauth_settings = optional(object({
+      status           = optional(string, "DISABLED")
+      data_source_name = optional(string, null)
+      secret_arn       = optional(string, null)
+    }), null)
+    kendra_settings = optional(object({
+      status = optional(string, null)
+    }), null)
   })
+  default = {
+    model_register_settings          = null
+    time_series_forecasting_settings = null
+    workspace_settings               = null
+    identity_provider_oauth_settings = {
+      status = "DISABLED"
+    }
+  }
+}
+
+variable "r_studio_server_pro_app_settings" {
+  type = object({
+    user_group    = optional(string, "R_STUDIO_USER")
+    access_status = optional(string, "DISABLED")
+  })
+  default = {
+    user_group = "R_STUDIO_USER"
+    ## (Optional) access_status 
+    ## Indicates whether the current user has access to the RStudioServerPro app. 
+    ## Valid values are ENABLED and DISABLED
+    access_status = "DISABLED"
+  }
 }
--- sagemaker/domain/02-resource.tf
+++ sagemaker/domain/02-resource.tf
@@ -5,18 +5,18 @@ locals {
 #### AWS Sagemaker Domain #######################################################################################################
 resource "aws_sagemaker_domain" "default" {
   # for_each                = { for domain in var.domain : domain.domain_name => var.domain }
-  for_each                = toset([var.domain.domain_name])
+  for_each                = toset([var.domain_name])
   domain_name             = each.key
-  auth_mode               = var.domain.auth_mode
-  app_network_access_type = var.domain.app_network_access_type
-  vpc_id                  = var.vpc_id[var.domain.vpc_identifier]
-  subnet_ids              = [for sub_identifier in var.domain.subnet_identifiers : var.sub_id[sub_identifier]]
+  auth_mode               = var.auth_mode
+  app_network_access_type = var.app_network_access_type
+  vpc_id                  = var.vpc_id[var.vpc_identifier]
+  subnet_ids              = [for sub_identifier in var.subnet_identifiers : var.sub_id[sub_identifier]]
   # app_security_group_management
   # domain_settings
   # kms_key_id
   # retention_policy
   default_user_settings {
-    execution_role = var.domain.execution_role
+    execution_role = var.execution_role
     # security_groups = 
     sharing_settings {
 
@@ -29,16 +29,27 @@ resource "aws_sagemaker_domain" "default" {
     }
     canvas_app_settings {
       model_register_settings {
-        # cross_account_model_register_role_arn = 
-        # status      = 
+        status                                = try(var.canvas_app_settings.model_register_settings.status, null)
+        cross_account_model_register_role_arn = try(var.canvas_app_settings.model_register_settings.cross_account_model_register_role_arn, null)
       }
       time_series_forecasting_settings {
-        #   amazon_forecast_role_arn = 
-        #   status = 
+        status                   = try(var.canvas_app_settings.time_series_forecasting_settings.status, null)
+        amazon_forecast_role_arn = try(var.canvas_app_settings.time_series_forecasting_settings.amazon_forecast_role_arn, null)
       }
       workspace_settings {
-        #   s3_artifact_path = 
-        #   s3_kms_key_id = 
+        s3_artifact_path = try(var.canvas_app_settings.workspace_settings.s3_artifact_path, null)
+        s3_kms_key_id    = try(var.canvas_app_settings.workspace_settings.s3_kms_key_id, null)
+      }
+      direct_deploy_settings {
+        status = try(var.canvas_app_settings.direct_deploy_settings.status, null)
+      }
+      # identity_provider_oauth_settings {
+      #   status           = var.canvas_app_settings.identity_provider_oauth_settings.status
+      #   secret_arn       = var.canvas_app_settings.identity_provider_oauth_settings.status == "DISABLED" ? null : var.canvas_app_settings.identity_provider_oauth_settings.secret_arn
+      #   data_source_name = var.canvas_app_settings.identity_provider_oauth_settings.status == "DISABLED" ? null : var.canvas_app_settings.identity_provider_oauth_settings.data_source_name
+      # }
+      kendra_settings {
+        status = try(var.canvas_app_settings.kendra_settings.status, null)
       }
     }
     tensor_board_app_settings {
@@ -48,9 +59,8 @@ resource "aws_sagemaker_domain" "default" {
 
     }
     r_studio_server_pro_app_settings {
-      # model_register_settings
-      # time_series_forecasting_settings
-      # workspace_settings
+      user_group    = try(var.r_studio_server_pro_app_settings.user_group, null)
+      access_status = try(var.r_studio_server_pro_app_settings.access_status, null)
     }
   }
 }
