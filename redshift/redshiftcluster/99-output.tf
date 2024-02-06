44ffd6e5fec22285d8c3fccc9c9b326eb0725a26
lsj <leesj03298@naver.com>
2024-02-06 17:33:29 +0900

    redshift module output update

--- redshift/redshiftcluster/99-output.tf
+++ redshift/redshiftcluster/99-output.tf
@@ -6,10 +6,26 @@ output "dns_name" {
   value = { for key, value in aws_redshift_cluster.default : key => value.dns_name }
 }
 
-output "dns_node_type" {
+output "node_type" {
   value = { for key, value in aws_redshift_cluster.default : key => value.node_type }
 }
 
-output "dns_number_of_nodes" {
+output "number_of_nodes" {
   value = { for key, value in aws_redshift_cluster.default : key => value.number_of_nodes }
+}
+
+output "publicly_accessible" {
+  value = { for key, value in aws_redshift_cluster.default : key => value.publicly_accessible }
+}
+
+output "availability_zone_relocation_enabled" {
+  value = { for key, value in aws_redshift_cluster.default : key => value.availability_zone_relocation_enabled }
+}
+
+output "default_iam_role_arn" {
+  value = { for key, value in aws_redshift_cluster.default : key => value.default_iam_role_arn }
+}
+
+output "iam_roles" {
+  value = { for key, value in aws_redshift_cluster.default : key => value.iam_roles }
 }
